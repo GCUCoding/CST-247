0896d8248dd8954f4ba2b287aab2c2b40a8ffd17
GCUCoding <77521006+GCUCoding@users.noreply.github.com>
2021-01-29 20:22:16 -0700

    frontend

---
+++ Milestone Project/Controllers/MinesweeperController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Milestone_Project.Controllers
+{
+    public class MinesweeperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
