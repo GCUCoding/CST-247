3080988d35dd588b855c7cce515b96435642212e
Max1mus7 <maxdweber7@gmail.com>
2021-01-29 21:13:46 -0700

    Data/Business Layers

---
+++ Milestone Project/Models/UserInfo/UserData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer;
+using DataAccessLayer;
+
+namespace Milestone_Project.Models.UserInfo
+{
+    public class UserData
+    {
+        UserDataAccess dataAccess = new UserDataAccess();
+
+        public IEnumerable<User> GetUsers()
+        {
+            return dataAccess.GetUsers();
+        }
+
+        public bool AddUser(User user)
+        {
+            return dataAccess.AddUser(user);
+        }
+    }
+}
